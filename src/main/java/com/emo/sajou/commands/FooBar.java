7a93dc8147f3d2d81373350506e5d6acb1e09b15
cedricbou <cedricb1101@gmail.com>
2013-01-23 01:08:09 +0100

    Updated jackson from 1.9.9 to 2.1.1

--- src/main/java/com/emo/sajou/commands/FooBar.java
+++ src/main/java/com/emo/sajou/commands/FooBar.java
@@ -2,8 +2,8 @@ package com.emo.sajou.commands;
 
 import net.sf.oval.constraint.Length;
 
-import org.codehaus.jackson.annotate.JsonCreator;
-import org.codehaus.jackson.annotate.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class FooBar {
 
